db9900e50502257c8b823d3d450b75a8bf15a626
as <allensnape@qq.com>
2023-12-03 16:02:33 +0800

    feat: 1

---
+++ main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"os/signal"
+	"syscall"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"periph.io/x/conn/v3/gpio"
+	"periph.io/x/conn/v3/gpio/gpioreg"
+	"periph.io/x/host/v3"
+)
+
+var HostAddr = "0.0.0.0:8080"
+
+// var PowerPin = "78"
+// var LightPin = "76"
+
+var OperatedPins = []string{}
+
+func CleanUpGPIO() {
+	for _, pin := range OperatedPins {
+		err := exec.Command(
+			"echo",
+			pin,
+			">",
+			"/sys/class/gpio/unexport",
+		).Run()
+		if err != nil {
+			log.Println("Failed to clean up pin", pin, ", because (of)", err)
+		} else {
+			log.Println("Clean up GPIO", pin)
+		}
+	}
+}
+
+func SetupGPIO() {
+	state, err := host.Init()
+	if err != nil {
+		panic(err)
+	}
+	log.Printf("\nCurrent State: %v \n", state)
+
+	// all := gpioreg.All()
+	// for index, pin := range all {
+	// 	state := gpio.Low
+	// 	for i := 0; i < 4; i++ {
+	// 		fmt.Printf("Set #%v/%v to %v\n", index, pin, state)
+	// 		err := pin.Out(state)
+	// 		if err != nil {
+	// 			fmt.Printf("Unable to set %v to %v because (of) %v\n", pin, state, err)
+	// 			break
+	// 		}
+	// 		time.Sleep(500 * time.Millisecond)
+	// 		state = !state
+	// 	}
+	// }
+
+	// p := gpioreg.ByName("78")
+	// t := time.NewTicker(3 * time.Second)
+	// for l := gpio.Low; ; l = !l {
+	// 	err := p.Out(l)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// 	fmt.Printf("Set %v to %v\n", p, l)
+	// 	<-t.C
+	// }
+}
+
+func SetupHTTPServer() {
+	router := gin.Default()
+	router.Use(cors.Default())
+
+	router.PUT("/pin/:pin/:status", func(ctx *gin.Context) {
+		pinNumber := ctx.Params.ByName("pin")
+		statusStr := ctx.Params.ByName("status")
+		status := gpio.Low
+		if statusStr == "1" {
+			status = gpio.High
+		}
+		pin := gpioreg.ByName(pinNumber)
+		if pin == nil {
+			ctx.JSON(http.StatusNotFound, false)
+			return
+		}
+		err := pin.Out(status)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, err)
+			return
+		}
+		OperatedPins = append(OperatedPins, pinNumber)
+		ctx.JSON(http.StatusOK, true)
+	})
+
+	router.Run(HostAddr)
+}
+
+func SetupCtrlC() {
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+	sig := <-sigChan
+	log.Println("\nReceived signal:", sig)
+	CleanUpGPIO()
+	os.Exit(0)
+}
+
+func main() {
+	SetupGPIO()
+	go SetupHTTPServer()
+	SetupCtrlC()
+}
